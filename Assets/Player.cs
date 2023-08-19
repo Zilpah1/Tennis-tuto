439448695c70bf1f28b8d8a9ed43764977ca6e0a
Zilpah1 <zilpahnamutali1@gmail.com>
2023-08-19 20:11:01 +0300

    First commit

---
+++ Assets/Player.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+    public Transform aimTarget;
+    float speed = 3f;
+    float force = 500;
+
+    bool hitting;
+
+ 
+    //Update is called once per frame
+    void Update()
+    {
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        
+
+        if( Input.GetKeyDown(KeyCode.F))
+        {
+            hitting = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.F))
+        {
+            hitting = false;
+        }
+        
+        if(hitting)
+        {
+            aimTarget.Translate(new Vector3(h , 0 , 0) * speed * Time.deltaTime);
+        }
+
+        if ( (h != 0 || v != 0 ) && !hitting)
+        {
+            transform.Translate(new Vector3(h , 0 , v) * speed * Time.deltaTime);
+        }
+
+    }
+
+    private void OntriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Ball"))
+        {
+            Vector3 dir = aimTarget.position - transform.position;
+            other.GetComponent<Rigidbody>().velocity = dir.normalized * force + new Vector3(0 , 6 , 0 );
+        }
+    }
+
+
+}
+
+
+
+
